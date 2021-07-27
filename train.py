f26a7f4365dfa4cf75cdd200de8c4583ab4e0525
Pooya <pfayyazs@gmu.edu>
2021-07-27 10:23:11 -0400

    train/test loop

---
+++ train.py
@@ -0,0 +1,76 @@
+from model import *
+from data_loader import PoseDataset
+
+num_epochs = 500
+batch_size = 20
+learning_rate = 0.00001
+num_bins = 8
+in_channels = 64
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print ('device', device)
+
+train_dataset = ASLDataset(input_path, "train")
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=6)
+
+test_dataset = ASLDataset(input_path, "test")
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=6)
+
+model = PoseEstimationModel(in_channels, num_bins)
+model.to(device)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(list(model.parameters()), lr=learning_rate)
+n_total_steps = len(train_loader)
+scheduler = StepLR(optimizer, step_size=step_size, gamma=gamma)
+
+for epoch in range(num_epochs):
+	for i, (inputs, labels) in enumerate(train_dataloader):
+		
+		print(inputs.shape())
+		inputs = inputs.to(device)
+		labels = labels.to(device)
+
+		model.train()
+
+		optimizer.zero_grad()
+		# compute the model output
+		yhat = model(inputs)
+		# calculate loss
+		loss = criterion(yhat, labels)
+		# credit assignment
+		loss.backward()
+		# update model weights
+		optimizer.step()
+
+        if (i+1) % 5 == 0:
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+            
+	for i, (inputs, labels) in enumerate(test_dataloader):
+		inputs = inputs.to(device)
+		labels = labels.to(device)
+
+		optimizer.zero_grad()
+		yhat = model(inputs)
+		loss = criterion(yhat, labels.long())
+		_, predicted = torch.max(yhat.data, 1)
+
+        	# Total number of labels
+		total += labels.size(0)
+
+		if torch.cuda.is_available():
+			correct += (predicted.cpu() == labels.cpu()).sum()
+		else:
+			correct += (predicted == labels).sum()
+    
+	accuracy = 100 * correct / total
+	print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {train_loss.item():.4f}, Val_Accuracy [{accuracy}]')
+		
+	scheduler.step()
+	
+print('Finished Training')
+PATH = './LSTM.pth'
+torch.save(model.state_dict(), PATH)
+
+
+
