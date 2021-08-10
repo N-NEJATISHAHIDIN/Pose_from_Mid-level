a92ad2c5c4f874a1861a98bb64f5d541930d08dd
Negar <nnejatis@gmu.edu>
2021-08-10 18:29:45 -0400

    Model MaskedFeatures, Upsample MaskedFeatures, and Upsample MaskChannel are added

--- data_loader.py
+++ data_loader.py
@@ -13,12 +13,12 @@ import subprocess
 
 class PoseDataset(torch.utils.data.Dataset):
     
-    def __init__(self, path, list_ids, labels):
+    def __init__(self, path, list_ids, labels, mask_size):
         
         self.labels = labels
         self.list_ids = list_ids
         self.path = path
-
+        self.mask_size = mask_size
         
     def __len__(self):
         
@@ -31,8 +31,8 @@ class PoseDataset(torch.utils.data.Dataset):
         # X = Image.open(self.path  + "/Pix3D/crop/" + ID[4:].split(".")[0]+".png")
         Z = read_image(self.path  + "/Pix3D/crop_mask/" + ID[4:].split(".")[0]+".png")
         # ,interpolation = Image.NEAREST
-        mask = transforms.Resize((16,16))(Z)
-        out = mask[0].reshape(1,16,16)
+        mask = transforms.Resize((self.mask_size,self.mask_size))(Z)
+        out = mask[0].reshape(1,self.mask_size,self.mask_size)
 
         edge_temp = torch.load(self.path  + '/Pix3D/featurs/normal/'+ID[4:].split(".")[0]+'.pt', map_location=torch.device('cpu'))
         normal_temp = torch.load(self.path  + '/Pix3D/featurs/edge_texture/'+ID[4:].split(".")[0]+'.pt', map_location=torch.device('cpu'))
@@ -41,6 +41,7 @@ class PoseDataset(torch.utils.data.Dataset):
         y =  torch.tensor((self.labels[self.labels.index.str.contains( "crop/"+ID[4:].split(".")[0])]).values[-3:])
         #edge_temp.float(), 
         try:
-            return torch.cat((normal_temp.float(), edge_temp.float(), out.float())), y[0][0]
+            return (torch.cat((normal_temp.float(), edge_temp.float())),out.float()), y[0][0]
+            # , out.float()
         except:
             print(y,ID)
--- train.py
+++ train.py
@@ -11,10 +11,10 @@ num_epochs = 20
 batch_size = 20
 learning_rate = 0.001
 num_bins = 8
-in_channels = 17
+in_channels = 16
 step_size = 2
 input_path = "../../Datasets/pix3d"
-
+mask_size =64
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print ('device', device)
 
@@ -34,14 +34,16 @@ raw_labels = pd.DataFrame(dict_pix3d[:,5:])
 labels  = generate_label(raw_labels, num_bins)
 
 
-train_dataset = PoseDataset(input_path, train_im_list, labels )
+train_dataset = PoseDataset(input_path, train_im_list, labels ,mask_size)
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 train_dataset[0]
 
-test_dataset = PoseDataset(input_path,test_im_list, labels)
+test_dataset = PoseDataset(input_path,test_im_list, labels,mask_size)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-model = PoseEstimationModel(in_channels, num_bins)
+model = PoseEstimationModelUpsampel_V1_MaskAsChannel(in_channels, num_bins, mask_size)
+# model = PoseEstimationModel_baseline(in_channels, num_bins)
+
 model.to(device)
 
 criterion = torch.nn.CrossEntropyLoss()
@@ -53,13 +55,15 @@ for epoch in range(num_epochs):
 	model.train()
 	for i, (inputs, labels) in enumerate(train_dataloader):
 		
-		inputs = inputs.to(device)
+		features = inputs[0].to(device)
+		mask = inputs[1].to(device)
+
 		labels = labels.to(device)
 		model.train()
 
 		optimizer.zero_grad()
 		# compute the model output
-		yhat = model(inputs)
+		yhat = model(features,mask,1)
 		# calculate loss
 		train_loss = criterion(yhat, labels)
 		# credit assignment
@@ -68,29 +72,33 @@ for epoch in range(num_epochs):
 		optimizer.step()
 
 	total = 0  
-	correct = 0  	
+	correct = 0 
+	correct2 = 0 	
 	model.eval() 
 	for i, (inputs, labels) in enumerate(test_dataloader):
-		inputs = inputs.to(device)
+		features = inputs[0].to(device)
+		mask = inputs[1].to(device)
 		labels = labels.to(device)
 
 		optimizer.zero_grad()
-		yhat = model(inputs)
+		yhat = model(features, mask, 1)
 		#print("yhat : ",yhat)
 		#print("labels : ", labels)
 		test_loss = criterion(yhat, labels.long())
 		_, predicted = torch.max(yhat.data, 1)
-		#_, predicted = torch.topk(yhat.data, 3, 1)
+		_, predicted2 = torch.topk(yhat.data, 2, 1)
 		
         # Total number of labels
 		total += labels.size(0)
 		
 
 		correct += torch.sum(predicted == labels).item()
-		#correct += torch.sum(torch.eq(predicted, labels.reshape(-1,1))).item()
+		correct2 += torch.sum(torch.eq(predicted2, labels.reshape(-1,1))).item()
     
 	accuracy = 100 * correct / total
-	print (f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss.item():.4f}, Test Loss: {test_loss.item():.4f}, Val_Accuracy [{accuracy}]')
+	accuracy2 = 100 * correct2 / total
+
+	print (f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss.item():.4f}, Test Loss: {test_loss.item():.4f}, Val_Accuracy [{accuracy}], Val_Accuracy2 [{accuracy2}]')
 		
 	scheduler.step()
 	
