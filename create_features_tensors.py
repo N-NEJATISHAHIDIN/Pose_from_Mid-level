2d3a75925f51a444eca97d27ce8b14519e9423ac
Pooya <pfayyazs@gmu.edu>
2021-08-12 16:10:43 -0400

    add classification report per category

---
+++ create_features_tensors.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import torchvision.transforms.functional as TF
+import visualpriors
+import subprocess
+from torchvision.io import read_image
+import torch
+import pandas as pd
+import numpy as np
+import os
+from tqdm import tqdm
+
+
+features = ['autoencoding','depth_euclidean','jigsaw' ,'reshading','colorization',
+'edge_occlusion','keypoints2d','room_layout',
+'curvature'  ,'keypoints3d'  ,'segment_unsup2d'  ,
+'class_object' ,'egomotion' ,  'nonfixated_pose'   , 'segment_unsup25d',
+'class_scene',  'fixated_pose'  , 'segment_semantic',      
+'denoising' , 'inpainting'   ,'point_matching' ,   'vanishing_point'
+]
+
+csv_file = open("/home/negar/Documents/Datasets/pix3d/Pix3D/Pix3D.txt")
+data_f = pd.read_csv(csv_file)
+
+all_categories = set(data_f["cat_id"])
+
+
+for feat in features:
+    os.mkdir("/home/negar/Documents/Datasets/pix3d/Pix3D/featurs/"+feat)
+    for i in all_categories :
+            os.mkdir("/home/negar/Documents/Datasets/pix3d/Pix3D/featurs/"+ feat + "/"+ i)
+
+
+for feat in tqdm(features):
+    for ID in data_f["image_path"]:
+        image = Image.open('../../Datasets/pix3d/Pix3D/'+ID.split(".")[0]+".png")
+        
+        x = TF.to_tensor(TF.resize(image, 256)) * 2 - 1
+        x = (x.unsqueeze_(0)).cuda()
+
+        midlevel_feats = (visualpriors.representation_transform(x, 'edge_texture', device='cuda:0'))[0]
+        #normal_temp = (visualpriors.representation_transform(x, 'normal', device='cuda:0'))[0]
+        
+        torch.save(midlevel_feats, "/home/negar/Documents/Datasets/pix3d/Pix3D/featurs/"+feat+ID[4:].split(".")[0]+".pt")
+        #torch.save(normal_temp , "../../../Datasets/pix3d/Pix3D/featurs/normal"+ID[4:].split(".")[0]+".pt")
+
--- train.py
+++ train.py
@@ -5,103 +5,136 @@ import pandas as pd
 import torch
 from utills import *
 from torch.optim.lr_scheduler import StepLR
+from sklearn.metrics import classification_report
+from collections import Counter
+
 
 num_workers = 6
-num_epochs = 20
+num_epochs = 9
 batch_size = 20
 learning_rate = 0.001
 num_bins = 8
-in_channels = 16
-step_size = 2
+in_channels = 24
+step_size = 3
 input_path = "../../Datasets/pix3d"
 mask_size =64
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print ('device', device)
+
+#features = ['autoencoding','depth_euclidean','jigsaw' ,'reshading','colorization',
+#'edge_occlusion','keypoints2d','room_layout',
+#'curvature'  ,'keypoints3d'  ,'segment_unsup2d'  ,
+#'class_object' ,'egomotion' ,  'nonfixated_pose'   , 'segment_unsup25d',
+#'class_scene',  'fixated_pose'  , 'segment_semantic',      
+#'denoising' , 'inpainting'   ,'point_matching' ,   'vanishing_point'
+#]
+
+features = ['jigsaw']
 
 
-test_dict = eval(open("pix3d_s1_test.json",mode='r',encoding='utf-8').read())
-test_im_list = pd.DataFrame(test_dict["images"])["file_name"]
 
-train_dict = eval(open("pix3d_s1_train.json",mode='r',encoding='utf-8').read())
-train_im_list = pd.DataFrame(train_dict["images"])["file_name"]
+for feature in features:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        print ('device', device)
 
-csv_file = open(input_path + "/Pix3D/Pix3D.txt")
-data_f = pd.read_csv(csv_file)
-#all infor about all imgs in all categories
-dict_pix3d = np.asarray(data_f)
-raw_labels = pd.DataFrame(dict_pix3d[:,5:])
+        test_dict = eval(open("pix3d_s1_test.json",mode='r',encoding='utf-8').read())
+        test_im_list = pd.DataFrame(test_dict["images"])["file_name"]
 
-labels  = generate_label(raw_labels, num_bins)
+        train_dict = eval(open("pix3d_s1_train.json",mode='r',encoding='utf-8').read())
+        train_im_list = pd.DataFrame(train_dict["images"])["file_name"]
+
+        csv_file = open(input_path + "/Pix3D/Pix3D.txt")
+        data_f = pd.read_csv(csv_file)
+        #all infor about all imgs in all categories
+        dict_pix3d = np.asarray(data_f)
+        raw_labels = pd.DataFrame(dict_pix3d[:,5:])
+
+        labels  = generate_label(raw_labels, num_bins)
+        print(labels)
+        print("The "+ feature + " has been added.")
+        train_dataset = PoseDataset(input_path, train_im_list, labels ,mask_size, feature)
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        # train_dataset[0]
+
+        test_dataset = PoseDataset(input_path,test_im_list, labels,mask_size, feature)
+        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
 
-train_dataset = PoseDataset(input_path, train_im_list, labels ,mask_size)
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-train_dataset[0]
+        model = PoseEstimationModelUpsampel_V1_MaskedFeatures(in_channels, num_bins, mask_size)
+        # model = PoseEstimationModelUpsampel_V1_MaskAsChannel(in_channels, num_bins, mask_size)
+        # model = PoseEstimationModel_baseline(in_channels, num_bins)
 
-test_dataset = PoseDataset(input_path,test_im_list, labels,mask_size)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        model.to(device)
 
-model = PoseEstimationModelUpsampel_V1_MaskAsChannel(in_channels, num_bins, mask_size)
-# model = PoseEstimationModel_baseline(in_channels, num_bins)
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(list(model.parameters()), lr=learning_rate)
+        n_total_steps = len(train_dataloader)
+        scheduler = StepLR(optimizer, step_size=step_size)
 
-model.to(device)
+        for epoch in range(num_epochs):
+                model.train()
+                for i, (inputs, labels, _) in enumerate(train_dataloader):
+                        
+                        features = inputs[0].to(device)
+                        mask = inputs[1].to(device)
 
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(list(model.parameters()), lr=learning_rate)
-n_total_steps = len(train_dataloader)
-scheduler = StepLR(optimizer, step_size=step_size)
+                        labels = labels.to(device)
+                        model.train()
 
-for epoch in range(num_epochs):
-	model.train()
-	for i, (inputs, labels) in enumerate(train_dataloader):
-		
-		features = inputs[0].to(device)
-		mask = inputs[1].to(device)
+                        optimizer.zero_grad()
+                        # compute the model output
+                        yhat = model(features,mask,0)
+                        # calculate loss
+                        train_loss = criterion(yhat, labels)
+                        # credit assignment
+                        train_loss.backward()
+                        # update model weights
+                        optimizer.step()
 
-		labels = labels.to(device)
-		model.train()
+                total = 0  
+                correct = 0 
+                correct2 = 0    
+                model.eval()
+                all_labels = []
+                all_pred = []
+                all_cls = []
+                for i, (inputs, labels, cls) in enumerate(test_dataloader):
+                        features = inputs[0].to(device)
+                        mask = inputs[1].to(device)
+                        labels = labels.to(device)
+                        
+                        optimizer.zero_grad()
+                        yhat = model(features, mask, 0)
+                        #print("yhat : ",yhat)
+                        #print("labels : ", labels)
+                        test_loss = criterion(yhat, labels.long())
+                        _, predicted = torch.max(yhat.data, 1)
+                        _, predicted2 = torch.topk(yhat.data, 2, 1)
+                        
+                        #print(labels.cpu().tolist())
+                        all_labels.extend(labels.cpu().tolist())
+                        all_pred.extend(predicted.cpu().tolist())
+                        all_cls.extend(list(cls))
 
-		optimizer.zero_grad()
-		# compute the model output
-		yhat = model(features,mask,1)
-		# calculate loss
-		train_loss = criterion(yhat, labels)
-		# credit assignment
-		train_loss.backward()
-		# update model weights
-		optimizer.step()
+                        # Total number of labels
+                        total += labels.size(0)
+                        
+                        correct += torch.sum(predicted == labels).item()
+                        correct2 += torch.sum(torch.eq(predicted2, labels.reshape(-1,1))).item()
+            
+                accuracy = 100 * correct / total
+                accuracy2 = 100 * correct2 / total
+                
+                print("############################################################################################")
+                print (f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss.item():.4f}, Test Loss: {test_loss.item():.4f}, Val_Accuracy [{accuracy}], Val_Accuracy2 [{accuracy2}]')
+                print(classification_report(all_labels, all_pred))
+                
+                d2 = (Counter(np.array(all_cls)[np.array(all_labels) ==  np.array(all_pred)]))
+                d1 = (Counter(all_cls))
+                print(d1) 
+                d3 = dict((k, "%.2f" % ( (float(d2[k]) / d1[k])*100 )) for k in d2)
+                print(d3)
 
-	total = 0  
-	correct = 0 
-	correct2 = 0 	
-	model.eval() 
-	for i, (inputs, labels) in enumerate(test_dataloader):
-		features = inputs[0].to(device)
-		mask = inputs[1].to(device)
-		labels = labels.to(device)
-
-		optimizer.zero_grad()
-		yhat = model(features, mask, 1)
-		#print("yhat : ",yhat)
-		#print("labels : ", labels)
-		test_loss = criterion(yhat, labels.long())
-		_, predicted = torch.max(yhat.data, 1)
-		_, predicted2 = torch.topk(yhat.data, 2, 1)
-		
-        # Total number of labels
-		total += labels.size(0)
-		
-
-		correct += torch.sum(predicted == labels).item()
-		correct2 += torch.sum(torch.eq(predicted2, labels.reshape(-1,1))).item()
-    
-	accuracy = 100 * correct / total
-	accuracy2 = 100 * correct2 / total
-
-	print (f'Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss.item():.4f}, Test Loss: {test_loss.item():.4f}, Val_Accuracy [{accuracy}], Val_Accuracy2 [{accuracy2}]')
-		
-	scheduler.step()
-	
-print('Finished Training')
-PATH = './LSTM.pth'
-torch.save(model.state_dict(), PATH)
+                scheduler.step()
+                
+        print('Finished Training')
+        PATH = './LSTM.pth'
+        torch.save(model.state_dict(), PATH)
